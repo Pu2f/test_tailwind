c9a168127c4bd81793272d3c14b34f8ae804f2c0
Pu2f <chatchanok.nt@gmail.com>
2026-02-11 10:44:32 +0700

    modified main

--- my-flask-web/blueprints/main.py
+++ my-flask-web/blueprints/main.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint("main", __name__)
 
-@main_bp.route('/')
+
+@main_bp.route("/")
 def index():
-    return render_template('main/index.html')
+    return render_template("main/index.html")
